26642a5b60f06e6a13ac47b950268d1ed198c619
sophiabonsu <sophiabonsu@gmail.com>
2019-07-17 13:50:55 -0400

    second commit

--- src/App.js
+++ src/App.js
@@ -5,21 +5,79 @@ import React,{Component} from 'react';
      super();
      this.state ={
        counter:0,
+       double: false,
      }
     }
+
+  increment = () => {
+    if (this.state.counter < 20){
+      this.setState({
+        counter :this.state.counter + 1
+      })
+    }
+    if (this.state.double){
+      if(this.state.counter <19){
+        this.setState({
+          counter :this.state.counter + 2
+        })
+      }
+    }
+  };
+  
+  decrement = () =>{
+    if(this.state.counter >0){
+      this.setState({
+        counter: this.state.counter - 1,
+      })
+    }
+    if(this.state.double){
+      if(this.state.counter >1){
+        this.setState({
+          counter:this.state.counter -2
+        })
+      }
+    }
+  }
+   clear = () =>{
+     this.setState({
+       counter:0,
+     })
+   }
+
+   double =() =>{
+     if(this.state.double){
+       this.setState({
+         double:false,
+       })
+     }
+     else{
+       this.setState({
+         double:true
+       })
+     }
+   }
+
+
+
+
    render(){
      
      console.log("This line 13 representing state", this.counter);
+     console.log("This is the double value", this.state.double);
      return(
        <div className="container">
          <div className ="navbar">Counter.js</div>
          <div className='counter'>
-           <h1>Something here?</h1>
-           <button type ="button"Increment></button>
-           <button type ="button"Decrement></button>
+           <h1>{this.state.counter}</h1>
+           <button type ="button" onClick={this.clear}>Clear Count</button>
+           <button type ="button" onClick={this.increment}>Increment</button>
+           <button type ="button" onClick={this.decrement}>Decrement</button>
+           <button type ="button" onClick={this.double}>{this.state.double ? 'Double Count': "Single Count"}</button>
          </div>
        </div>
      )
 
    }
  }
+
+ export default Counter;
